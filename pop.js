c4f68e229d7a9f33147e974f7ae316858de793f1
Noway <vadimnikig@mail.ru>
2022-08-27 19:37:57 +0400

    first commit

---
+++ pop.js
@@ -0,0 +1,9 @@
+
+//const result = confirm("Are you ready?");
+//if (result == 0){
+//    alert("hello world");
+//}
+const answer = prompt("Skok let", "");
+if (answer >= 17){
+    alert(`${answer}`);
+}
